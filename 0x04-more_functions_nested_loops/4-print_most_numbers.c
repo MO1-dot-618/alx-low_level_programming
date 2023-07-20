f44ef92da9b5d6a83886090187099f0a17b3219c
Malika <malikaoubilla@gmail.com>
2023-07-20 15:04:31 -0700

    nested

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_most_numbers - print from 0 to 9 except 2 and 4
+ *Return: void
+ **/
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i == 50 || i == 52)
+		{
+			continue;
+		}
+		_putchar(i);
+	}
+	_putchar('\n');
+}
