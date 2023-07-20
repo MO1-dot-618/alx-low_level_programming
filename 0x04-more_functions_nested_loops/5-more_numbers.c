9f2a2959163e79f1baf0a5d1959c09f6b744c8de
Malika <malikaoubilla@gmail.com>
2023-07-20 15:19:10 -0700

    nested

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,18 +6,20 @@
  **/
 void more_numbers(void)
 {
-	int i,j,left, right;
+	int i, j, left, right;
+
 	for (j = 0; j < 10; j++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			left= i/10;
-			right=i%10;
+			left = i / 10;
+			right = i % 10;
 			if (i >= 10)
 			{
-				_putchar(left+48);
+				_putchar(left + 48);
 			}
-			_putchar(right+48);
+			_putchar(right + 48);
 		}
 		_putchar('\n');
+	}
 }
