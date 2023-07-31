62998d4d3d2d5fa25ee436a9587b9fb8a1504176
Malika <malikaoubilla@gmail.com>
2023-07-31 07:10:48 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strpbrk - search character in string
+ * @s: pointer to a string
+ * @accept: pointer to characters to find
+ * Return: pointer
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, n = 0;
+
+	while (*s != '\0')
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (NULL);
+}
