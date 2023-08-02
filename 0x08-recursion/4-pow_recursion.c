fadfe0b3d823ddc014678d222b304c27892ac170
root <malikaoubilla@gmail.com>
2023-08-02 05:31:30 -0700

    pow

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,20 +8,17 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (x < 0)
+	if (y < 0)
 	{
 		return (-1);
-	}
-	else if (x == 0)
-	{
-		return (x);
-	}
-	else if (y > 0)
-	{
-		return (x * _pow_recursion(x, y - 1));
 	}
 	else if (y == 0)
 	{
 		return (1);
 	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	
 }
