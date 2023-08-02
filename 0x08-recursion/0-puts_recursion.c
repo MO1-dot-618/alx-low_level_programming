7b15f54134989d1331fbaf6bdea4910c95f609f2
root <malikaoubilla@gmail.com>
2023-08-02 02:42:11 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,11 +5,13 @@
  * Return: void
  */
 void _puts_recursion(char *s)
-{
-	_putchar(*s);
-	s++;
+{	
 	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
 		_puts_recursion(s);
+	}
 	else
 		_putchar('\n');
 }
