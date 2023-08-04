d775124247a2f35df3f8c6a573a9d72a3e6e899e
root <malikaoubilla@gmail.com>
2023-08-04 08:18:20 -0700

    argc

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiply 2 arguments
+ * @argc: int
+ * @argv: string
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n",atoi(argv[1])*atoi(argv[2]));
+		return (0);
+	}
+}
