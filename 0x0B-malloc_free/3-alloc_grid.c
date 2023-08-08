921ceaef6367046492cbcf0e5f11ddf21b107776
root <malikaoubilla@gmail.com>
2023-08-08 03:34:46 -0700

    grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - 2D array of int
+ * @width: int
+ * @height: int
+ * Return: pointer
+ */
+int **alloc_grid(int width, int height)
+{
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	int i, j;
+	int **arr = (int **)malloc(sizeof(int *) * height);
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = (int *)malloc(sizeof(int) * width);
+		if (arr[i] == NULL)
+		{
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+
+	return (arr);
+}
