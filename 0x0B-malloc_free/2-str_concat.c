be5e9341d07b34298f97a1f0e1074cff165d8f0c
root <malikaoubilla@gmail.com>
2023-08-08 03:09:08 -0700

    copy

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,9 +11,9 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, c1 = 0, c2 = 0;
 	char *arr;
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; s1 != NULL && s1[i] != '\0'; i++)
 		c1++;
-	for (i = 0; s2[i] != '\0'; i++)
+	for (i = 0; s2 != NULL && s2[i] != '\0'; i++)
 		c2++;
 
 	arr = malloc(sizeof(char) * (c1 + c2 + 1));
