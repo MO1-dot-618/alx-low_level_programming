59b45feec7eb859258cb06874fcea4f4ee5a3978
root <malikaoubilla@gmail.com>
2023-08-08 03:37:08 -0700

    grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -8,14 +8,15 @@
  */
 int **alloc_grid(int width, int height)
 {
+	int i, j;
+	int **arr;
+
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
 
-	int i, j;
-	int **arr = (int **)malloc(sizeof(int *) * height);
-
+	arr = (int **)malloc(sizeof(int *) * height);
 	if (arr == NULL)
 	{
 		return (NULL);
