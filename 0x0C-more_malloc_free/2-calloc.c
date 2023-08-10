00dcdeb63af56916ab47cb81c22752c41b094d98
root <malikaoubilla@gmail.com>
2023-08-10 03:57:09 -0700

    malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - allocate space for an array
+ * @size: unsigned int
+ * @c: char
+ * Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	if (size == 0 || nmemb == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		void *arr = malloc(nmemb * size);
+		unsigned int i;
+
+		if (arr == NULL)
+		{
+			return (NULL);
+		}
+		for (i = 0; i < nmemb * size; i++)
+		{
+			arr[i] = 0;
+		}
+		return (arr);
+	}
+}
