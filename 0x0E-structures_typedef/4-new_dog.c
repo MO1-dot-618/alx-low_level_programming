2f6458f3dd816571ed02056441a123786fbab3fc
root <malikaoubilla@gmail.com>
2023-08-14 09:52:51 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,39 @@
 #include "dog.h"
+#include <stdio.h>
+/**
+ * _strlen - returns the length of a string.
+ * @s: input string.
+ * Return: length of a string.
+ */
+int _strlen(char *s)
+{
+	int count = 0;
 
+	while (*(s + count) != '\0')
+		count++;
+	return (count);
+}
+/**
+ * _strcpy - copies the string pointed to by src,
+ * including the terminating null byte, to the
+ * buffer pointed to by dest.
+ * @dest: destination.
+ * @src: source.
+ * Return: the pointer to dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		*(dest + count) = *(src + count);
+		if (*(src + count) == '\0')
+			break;
+		count++;
+	}
+	return (dest);
+}
 /**
  * new_dog - create new dog
  * @name: The name of the dog - string
@@ -10,7 +44,6 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int i;
 	dog_t d;
 	dog_t *p = &d;
 
@@ -18,15 +51,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
-	else
-	{
-		for (i = 0; name[i] != '\0'; i++)
-			d.name[i] = name[i];
-		d.name[i] = '\0';
-		d.age = age;
-		for (i = 0; owner[i] != '\0'; i++)
-			d.owner[i] = owner[i];
-		d.owner[i] = '\0';
-	}
+	d.name = malloc(_strlen(name));
+	d.owner = malloc(_strlen(owner));
+	d.age = age;
+	_strcpy(d.name, name);
+	_strcpy(d.owner, owner);
 	return (p);
 }
