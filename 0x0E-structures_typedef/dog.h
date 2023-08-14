4fa39c83c6661b3289a8e29c9d1a466b313ca248
root <malikaoubilla@gmail.com>
2023-08-14 06:28:10 -0700

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  * struct dog - structure dog
+  * @name: char *
+  * @age: float
+  * @owner: char *
+  */
+struct dog
+{
+	char name[50];
+	float age;
+	char owner[50];
+};
+#endif
