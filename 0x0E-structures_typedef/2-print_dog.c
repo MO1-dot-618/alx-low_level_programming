8f77295f64936227841f89dde376aa4b682cc58e
root <malikaoubilla@gmail.com>
2023-08-14 08:26:46 -0700

    struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,26 +7,21 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d)
-	{
-		printf("Name: ");
-		if (d->name != NULL)
-		{
-			printf("%s\n", d->name);
-		}
-		else
-		{
-			printf("(nil)\n");
-		}
-		printf("Age: %.6f\n", d->age);
-		printf("Owner: ");
-		if (d->owner != NULL)
-		{
-			printf("%s\n", d->owner);
-		}
-		else
-		{
-			printf("(nil)\n");
-		}
-	}
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+
+	if (d->age < 0)
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -23,29 +23,6 @@ typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
-/**
- * _strcpy - copies the string pointed to by src,
- * including the terminating null byte, to the
- * buffer pointed to by dest.
- * @dest: destination.
- * @src: source.
- * Return: the pointer to dest.
- */
-char *_strcpy(char *dest, char *src)
-{
-	int count = 0;
-
-	while (count >= 0)
-	{
-		*(dest + count) = *(src + count);
-		if (*(src + count) == '\0')
-			break;
-		count++;
-	}
-	return (dest);
-}
-
+/* dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d); */
 #endif
