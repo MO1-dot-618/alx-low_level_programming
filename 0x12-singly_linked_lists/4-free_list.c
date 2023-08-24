3f7b6becf0498d25c8729afdc885e702c9e06a13
root <malikaoubilla@gmail.com>
2023-08-24 12:33:51 -0700

    task4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - free all lists
+ * @head: pointer to head node
+ * Return: no return
+ */
+void free_list(list_t *head)
+{
+	list_t *last;
+
+	while (head != NULL)
+	{
+		last = head;
+		head = head->next;
+		free(last->str);
+		free(last->next);
+	}
+}
