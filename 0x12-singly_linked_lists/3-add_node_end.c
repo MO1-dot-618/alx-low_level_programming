59b439b89227f2d3d53fe8a9ec74d07921444387
root <malikaoubilla@gmail.com>
2023-08-24 09:18:13 -0700

    task3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,7 +11,7 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *newNode;
+	list_t *newNode, *last = *head;;
 
 	newNode = malloc(sizeof(list_t));
 	if (newNode != NULL)
@@ -19,11 +19,16 @@ list_t *add_node_end(list_t **head, const char *str)
 		newNode->str = strdup(str);
 		newNode->len = strlen(str);
 		newNode->next = NULL;
-		while (*head != NULL)
+		if (*head == NULL)
 		{
-			(*head) = (*head)->next;
+			*head = newNode;
+			return (newNode);
 		}
-		*head = newNode;
+		while (last->next != NULL)
+		{
+			last = last->next;
+		}
+		last->next = newNode;
 	}
 	return (newNode);
 }
